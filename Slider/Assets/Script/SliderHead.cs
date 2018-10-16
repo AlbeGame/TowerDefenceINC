32520f369c4c110d883291bab8db78c7bc415816
Alberto <alberto.paparella.88@gmail.com>
2018-10-16 12:16:37 +0200

    New rendering based on late update

--- Slider/Assets/Script/SliderHead.cs
+++ Slider/Assets/Script/SliderHead.cs
@@ -1,26 +1,51 @@
-﻿using System.Collections;
+﻿using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
-public class SliderHead : MonoBehaviour {
-
+public class SliderHead : MonoBehaviour
+{
     LineRenderer lineRend;
+    CircleCollider2D coll;
+
+    List<SliderPath> tilesChecked = new List<SliderPath>();
     Vector3 lineStartPosition;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         lineRend = GetComponent<LineRenderer>();
-	}
+        coll = GetComponent<CircleCollider2D>();
+    }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void LateUpdate()
+    {
+        if (tilesChecked == null || tilesChecked.Count <= 0)
+            return;
+
+        lineRend.positionCount = tilesChecked.Count + 1;
+        lineRend.SetPosition(0, tilesChecked[0].GetOriginSideCenter());
+        for (int i = 1; i < tilesChecked.Count; i++)
+        {
+            lineRend.SetPosition(i, tilesChecked[i].transform.position);
+        }
+        lineRend.SetPosition(lineRend.positionCount -1, transform.position);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
         SliderPath sliderP = collision.GetComponent<SliderPath>();
-        if(sliderP != null)
+        if (tilesChecked.Contains(sliderP))
+            return;
+
+        if(Vector3.Distance(sliderP.transform.position, transform.position) < coll.radius)
         {
-            lineStartPosition = sliderP.GetOriginSideCenter();
-            lineRend.SetPosition(0, lineStartPosition);
-            lineRend.SetPosition(1, transform.position);
+            tilesChecked.Add(sliderP);
         }
+    }
+
+    private void OnMouseDrag()
+    {
+
     }
 }
