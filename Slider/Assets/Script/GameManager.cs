5f5711f8986ae034382f07c456ee259ca4fa20b4
Alberto <alberto.paparella.88@gmail.com>
2018-10-24 10:15:11 +0200

    GameManager as singleton

--- Slider/Assets/Script/GameManager.cs
+++ Slider/Assets/Script/GameManager.cs
@@ -5,22 +5,31 @@ namespace GameCore
 {
     public class GameManager : MonoBehaviour
     {
+        public static GameManager I { get; private set; }
+
         [SerializeField]
         private SliderHead sliderH;
         private SliderHead sliderH_I;
 
         private SliderPath sliderP_Start;
 
-        // Use this for initialization
-        void Start()
+        private void Awake()
         {
-
+            //--Singleton--
+            if (I != null)
+                DestroyImmediate(gameObject);
+            else
+            {
+                I = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            //-------------
         }
 
         void DeploySliderHead()
         {
             if (sliderH != null && sliderH_I == null)
-                sliderH_I = Instantiate<SliderHead>(sliderH);
+                sliderH_I = Instantiate<SliderHead>(sliderH,sliderP_Start.transform.position, sliderP_Start.transform.rotation);
         }
     }
 }
