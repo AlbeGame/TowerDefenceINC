b49fecd0e0f2636af74a16c3e4febde8a1bd7941
Alberto <alberto.paparella.88@gmail.com>
2018-10-29 10:01:45 +0100

    Removed Old project; New level change system

---
+++ Slider/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace GameCore
+{
+    public class LevelManager : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject playerPrefab;
+        GameObject playerInstance;
+        [SerializeField]
+        Transform startPosition;
+
+        void Start()
+        {
+            //Inject itself into GameManager
+            GameManager.I.LevelMng = this;
+
+            //Run level Setup
+            ResetLevel();
+        }
+
+        #region API
+        /// <summary>
+        /// Change scene by ID
+        /// </summary>
+        /// <param name="_lvlID"></param>
+        public void ChangeLevel(int _lvlID)
+        {
+            GameManager.I.ChangeLevel(_lvlID);
+        }
+
+        /// <summary>
+        /// Reset current level(/scene)
+        /// </summary>
+        public void ResetLevel()
+        {
+            if (playerInstance)
+                DestroyImmediate(playerInstance);
+
+            DeployPlayer();
+        }
+        #endregion
+
+        /// <summary>
+        /// Instantiate player
+        /// </summary>
+        void DeployPlayer()
+        {
+            if (playerPrefab != null)
+                playerInstance = Instantiate(playerPrefab, startPosition.position, startPosition.rotation);
+        }
+    }
+}
---
+++ Slider/Assets/Scripts/TargetTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace GameCore {
+    public class TargetTrigger : MonoBehaviour
+    {
+        public int NextLevelID;
+        LevelManager lvlMng;
+
+        // Use this for initialization
+        void Start()
+        {
+            lvlMng = FindObjectOfType<LevelManager>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            lvlMng.ChangeLevel(NextLevelID);
+        }
+    }
+}
