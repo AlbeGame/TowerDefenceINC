e936a061817ea8ec8b02f8c5ee99c13d1e068287
Alberto <alberto.paparella.88@gmail.com>
2018-10-30 12:31:10 +0100

    Level Design, partial

---
+++ Slider/Assets/Scripts/PistonBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+namespace GameCore {
+    public class PistonBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject dynamicElement;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        Tween motionTween;
+        private void OnMouseDown()
+        {
+            if (!motionTween.IsPlaying())
+            {
+                motionTween = dynamicElement.transform.DOMove(Vector3.one, 0.1f);
+                motionTween.OnComplete(() => motionTween.Rewind());
+            }
+        }
+    }
+}
